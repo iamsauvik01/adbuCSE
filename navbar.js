5aebfd07c0cd40df2a210837a3966188881abd2f
borishksh <nightfeury129@gmail.com>
2023-07-26 15:54:32 +0530

    Admission page done

---
+++ navbar.js
@@ -0,0 +1,27 @@
+document.write(`
+
+<nav>
+    <input type="checkbox" id="check" />
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+
+    </label>
+    <div class="logo-container">
+        <img src="./Images/Logo.png" alt="">
+        <div class="title">
+            <span style="font-weight: 700;">Assam Don Bosco University</span>
+            <span>Department of Computer Science</span>
+        </div>
+    </div>
+
+    <ul>
+        <li><a href="./index.html">Home</a></li>
+        <li><a href="./comingSoon.html">About</a></li>
+        <li><a href="./admission.html">Admission</a></li>
+        <li><a href="./comingSoon.html">Faculty</a></li>
+        <li><a href="./comingSoon.html">Placements</a></li>
+        <li><a href="./comingSoon.html">Labratory</a></li>
+        <li><a href="./comingSoon.html">Scholarships</a></li>
+    </ul>
+</nav>
+`);
