fa86a41945d02081354921e34708b90c9e4f1fe7
borishksh <nightfeury129@gmail.com>
2023-07-27 15:46:53 +0530

    Faculty page done

--- navbar.js
+++ navbar.js
@@ -18,7 +18,7 @@ document.write(`
         <li><a href="./index.html">Home</a></li>
         <li><a href="./comingSoon.html">About</a></li>
         <li><a href="./admission.html">Admission</a></li>
-        <li><a href="./comingSoon.html">Faculty</a></li>
+        <li><a href="./faculty.html">Faculty</a></li>
         <li><a href="./comingSoon.html">Placements</a></li>
         <li><a href="./comingSoon.html">Labratory</a></li>
         <li><a href="./comingSoon.html">Scholarships</a></li>
